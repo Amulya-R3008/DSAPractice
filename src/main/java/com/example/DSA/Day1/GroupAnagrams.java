88ea85e5a9d4f88cd002eed90053c6add5cb4dcc
Amulya R <amulya_r@epam.com>
2026-07-23 11:44:48 +0530

    group anagram

---
+++ src/main/java/com/example/DSA/Day1/GroupAnagrams.java
@@ -0,0 +1,55 @@
+package com.example.DSA.Day1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ *
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * Explanation:
+ *
+ * There is no string in strs that can be rearranged to form "bat".
+ * The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+ * The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+ * Example 2:
+ *
+ * Input: strs = [""]
+ *
+ * Output: [[""]]
+ *
+ * Example 3:
+ *
+ * Input: strs = ["a"]
+ *
+ * Output: [["a"]]
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            if (!map.containsKey(key)) {
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(str);
+
+        }return  new ArrayList<>(map.values());
+    }
+    public static void main(String[] args) {
+        GroupAnagrams g = new GroupAnagrams();
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(g.groupAnagrams(strs));
+    }
+}
